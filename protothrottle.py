5a206e32d6d8a6d7dc953c04432331e584cc2a8e
Nathan D. Holmes <ndholmes@fedex.com>
2021-04-21 21:12:42 -0600

    Commit what I've got of read-mode PT code

---
+++ protothrottle.py
@@ -0,0 +1,481 @@
+import mrbus
+from mrbus import packet
+import time
+
+def readEEPResponseValidate(txPkt, rxPkt):
+  if len(rxPkt.data) == (txPkt.data[2] + 2) and (rxPkt.data[0] + (rxPkt.data[1]*256)) == (txPkt.data[0] + (txPkt.data[1]*256)):
+    return True
+  return False
+
+def writeEEPResponseValidate(txPkt, rxPkt):
+  if rxPkt.data == txPkt.data:
+    return True
+
+  return False
+
+class protothrottle(object):
+  def __init__(self, bus):
+    self.bus = bus
+    self.throttleID = 'A'
+    self.throttleAddr = 0x30
+    self.version = None
+    self.slots = [{ } for i in range(20)]
+    self.globalConfig = { }
+    self.interpreter = None
+
+
+  def writeMemory(self, bus, targetAddr, memStart, data):
+    bytesWritten = 0
+    memLen = len(data)
+    
+    # 'W' can handle up to 12 data bytes at a time
+    while bytesWritten < memLen:
+      bytesToWrite = memLen    
+      if memLen >= 12:
+        bytesToWrite = 12
+
+      txPkt = packet(targetAddr, bus.addr, ord('W'), [memStart & 0xFF, memStart>>8] + data[bytesWritten:bytesWritten+bytesToWrite])
+      rxPkt = self.mrbusReliableTransact(bus, txPkt, responseValidate=writeEEPResponseValidate)
+
+      if None == rxPkt:
+        # apparently we timed out
+        raise IOError('timeout')
+
+      memStart = memStart + bytesToWrite
+      bytesWritten = bytesWritten + bytesToWrite
+      
+    
+  def readMemory(self, bus, targetAddr, memStart, memLen):
+    bytesRead = 0
+    data = []
+    
+    # 'R' can handle up to 12 data bytes at a time
+    while bytesRead < memLen:
+      bytesToRead = memLen    
+      if memLen >= 12:
+        bytesToRead = 12
+
+      txPkt = packet(targetAddr, bus.addr, ord('R'), [memStart & 0xFF, memStart>>8, bytesToRead])
+      rxPkt = self.mrbusReliableTransact(bus, txPkt, responseValidate=readEEPResponseValidate)
+
+      if None == rxPkt:
+        # apparently we timed out
+        raise IOError('timeout')
+
+      for i in range(2, 2+bytesToRead):
+        data.append(rxPkt.data[i])
+
+      memStart = memStart + bytesToRead
+      bytesRead = bytesRead + bytesToRead
+      
+    return bytes(data)
+
+  def mrbusReliableTransact(self, bus, txPkt, responseValidate=None, timeout=0.3, retries=5):
+    retry = retries
+    response = None
+
+    while retry > 0:
+      bus.sendpkt(txPkt.dest, [txPkt.cmd] + txPkt.data, txPkt.src)
+      now = start = time.time()
+      response = None
+
+      while now - start <= timeout:
+        rxPkt = bus.getpkt()
+        now = time.time()
+        if None == rxPkt:
+          continue
+            
+        #print("Got packet - src = 0x%02X, dest=0x%02X, cmd='%c', len(data)=%d" % (rxPkt.src, rxPkt.dest, rxPkt.cmd, len(rxPkt.data)))
+        #print(["0x%02X" % d for d in rxPkt.data])
+        
+        if rxPkt.dest != txPkt.src or rxPkt.src != txPkt.dest or rxPkt.cmd != (txPkt.cmd + 32): 
+          continue
+        
+        if None != responseValidate and not responseValidate(txPkt, rxPkt):
+          continue
+          
+        response = rxPkt
+        break
+
+      if None == response:
+        retry = retry - 1
+        continue
+
+      break
+
+    return response
+
+    
+    
+  def readVersion(self, targetAddr):
+    txPkt = packet(targetAddr, self.bus.addr, ord('V'), [])
+    rxPkt = self.mrbusReliableTransact(self.bus, txPkt)
+    
+    if None == rxPkt:
+      print("ERROR: Throttle not found")
+      return None
+
+    print("Throttle HW version %d.%d - Firmware Git Rev: %06x" % (rxPkt.data[4], rxPkt.data[5], 0xFFFFFF & ((rxPkt.data[1]<<16) + (rxPkt.data[2]<<8) + rxPkt.data[3])) )
+
+    mem = self.readMemory(self.bus, targetAddr, 0x0E, 2)
+    # The first versions of firmware didn't put an EEP layout number in place
+    if 0xFF == mem[0] and 0xFF == mem[1]:
+      mem[0] = 1
+      mem[1] = 0
+    print("Throttle reports eeprom layout %d.%d" % (mem[0], mem[1]) )
+
+    response = {
+      'hw_major':rxPkt.data[4],
+      'hw_minor':rxPkt.data[5],
+      'eep_major':mem[0],
+      'eep_minor':mem[1],
+      'gitrev': '%06x' % ((rxPkt.data[1]<<16) + (rxPkt.data[2]<<8) + rxPkt.data[3])
+    }
+
+    return response
+
+
+  def attach(self, throttleID):
+    self.throttleID = throttleID
+    self.throttleAddr = 0x30 + (ord(self.throttleID) - ord('A'))
+    self.version = self.readVersion(self.throttleAddr)
+    
+    if self.version == None:
+      raise IOError('Throttle Not Responding')
+    
+    
+    # Based on version, attach the correct interpreter
+    self.interpreter = pt_interpreter_v11()
+    self.interpreter.help()
+    self.slots = [{ } for i in range(20)]
+    self.globalConfig = { }
+
+
+
+  def readFromThrottle(self):
+    self.readGlobalConfig()
+    for slotNum in range(0,20):
+      self.readSlot(slotNum)
+
+
+  def readGlobalConfig(self):
+    memStart = 0x10
+    memLen = 0x70
+    confmem = self.readMemory(self.bus, self.throttleAddr, self.interpreter.globalConfMemStart, self.interpreter.globalConfMemLen)
+    self.globalConfig = self.interpreter.readGlobalConfig(confmem)
+
+  def readSlot(self, slotNum):
+    if slotNum > self.interpreter.numConfigSlots:
+      return
+
+    slotmem = self.readMemory(self.bus, self.throttleAddr, self.interpreter.getSlotStart(slotNum), self.interpreter.getSlotLen(slotNum))
+    self.slots[slotNum-1] = self.interpreter.readSlotConfig(slotmem)
+
+  def getSlot(self, slotNum):
+    if slotNum > self.interpreter.numConfigSlots:
+      return { }
+    return self.slots[slotNum-1]
+
+    
+    
+  def writeGlobalConfig(self):
+    memStart = 0x10
+    memLen = 0x70
+    # Read raw throttle configuration memory
+    # confmem = readMemory(mrbee, throttleAddr, 0x10, memLen)
+    # Canonicalize memory into dictionary to fit in global conf
+    # self.globalConfig = { } # FIXME
+    
+  def writeSlot(self, slotNum):
+    if slotNum > self.interpreter.numConfigSlots:
+      return
+
+    slotmem = self.interpreter.writeSlotMem(self.slots[slotNum-1])
+    self.writeMemory(self.bus, self.throttleAddr, self.interpreter.getSlotStart(slotNum), self.interpreter.getSlotLen(slotNum), slotmem)
+
+  
+class pt_interpreter_v11(object):
+
+  def __init__(self):
+    self.numConfigSlots     = 20
+    self.slotConfMemStart   = 0x80
+    self.slotConfMemLen     = 0x80
+    self.globalConfMemLen   = 0x16
+    self.globalConfMemStart = 0x10
+    self.functionXlate = {
+      'F00_MOM':0x00,
+      'F01_MOM':0x01,
+      'F02_MOM':0x02,
+      'F03_MOM':0x03,
+      'F04_MOM':0x04,
+      'F05_MOM':0x05,
+      'F06_MOM':0x06,
+      'F07_MOM':0x07,
+      'F08_MOM':0x08,
+      'F09_MOM':0x09,
+      'F10_MOM':0x0A,
+      'F11_MOM':0x0B,
+      'F12_MOM':0x0C,
+      'F13_MOM':0x0D,
+      'F14_MOM':0x0E,
+      'F15_MOM':0x0F,
+      'F16_MOM':0x10,
+      'F17_MOM':0x11,
+      'F18_MOM':0x12,
+      'F19_MOM':0x13,
+      'F20_MOM':0x14,
+      'F21_MOM':0x15,
+      'F22_MOM':0x16,
+      'F23_MOM':0x17,
+      'F24_MOM':0x18,
+      'F25_MOM':0x19,
+      'F26_MOM':0x1A,
+      'F27_MOM':0x1B,
+      'F28_MOM':0x1C,
+      'F00_LAT':0x40,
+      'F01_LAT':0x41,
+      'F02_LAT':0x42,
+      'F03_LAT':0x43,
+      'F04_LAT':0x44,
+      'F05_LAT':0x45,
+      'F06_LAT':0x46,
+      'F07_LAT':0x47,
+      'F08_LAT':0x48,
+      'F09_LAT':0x49,
+      'F10_LAT':0x4A,
+      'F11_LAT':0x4B,
+      'F12_LAT':0x4C,
+      'F13_LAT':0x4D,
+      'F14_LAT':0x4E,
+      'F15_LAT':0x4F,
+      'F16_LAT':0x50,
+      'F17_LAT':0x51,
+      'F18_LAT':0x52,
+      'F19_LAT':0x53,
+      'F20_LAT':0x54,
+      'F21_LAT':0x55,
+      'F22_LAT':0x56,
+      'F23_LAT':0x57,
+      'F24_LAT':0x58,
+      'F25_LAT':0x59,
+      'F26_LAT':0x5A,
+      'F27_LAT':0x5B,
+      'F28_LAT':0x5C,
+      'FN_OFF':0x80,
+      'FN_EMRG':0x81,
+      'FN_BRKTEST':0xC0 }
+
+
+  def getSlotStart(self, slotNum):
+    return self.slotConfMemStart + (slotNum-1) * self.slotConfMemLen
+
+  def getSlotLen(self, slotNum):
+    return self.slotConfMemLen
+
+  def funcToName(self, funcVal):
+    for name,val in self.functionXlate.items():
+      if funcVal == val:
+        return name
+    return 'FN_OFF'
+
+  def funcNameToValue(self, configSlotValues, name):
+    fn = 'FN_OFF'
+    if name in configSlotValues.keys() and configSlotValues[name] in self.functionXlate.keys():
+      fn = configSlotValues[name]
+
+    return self.functionXlate[fn]
+
+  def readGlobalConfig(self, mem):
+    globalConfValues = { }
+    
+    globalConfValues['sleepTimeMinutes'] = mem[0x01]
+    globalConfValues['fcDeadReckoningTime'] = mem[0x02]
+    globalConfValues['batteryOkayVolts'] = "%.1f" % (mem[0x04] / 10.0)
+    globalConfValues['batteryWarnVolts'] = "%.1f" % (mem[0x05] / 10.0)
+    globalConfValues['batteryCriticalVolts'] = "%.1f" % (mem[0x06] / 10.0)
+    globalConfValues['transmitHoldoffSeconds'] = "%.2f" % (mem[0x0D] / 100.0)
+    globalConfValues['timeSourceAddress'] = mem[0x0E]
+    globalConfValues['receiverAddress'] = mem[0x0F]
+    
+    globalConfValues['hornThreshold'] = mem[0x10]
+    globalConfValues['brakeThreshold'] = mem[0x11]
+    globalConfValues['brakeLowLimit'] = mem[0x12]
+    globalConfValues['brakeHighLimit'] = mem[0x13]
+    globalConfValues['brakePressureConfig'] = mem[0x14]
+    globalConfValues['alerterTimeoutSeconds'] = mem[0x15]
+    
+    return globalConfValues
+
+  def writeSlotConfig(self):
+    mem = [0xFF for i in range(self.slotConfMemLen)]
+
+    # Clean up the address and handle the case where it's either not all there
+    # or doesn't make any sense
+    addrVal = 3
+    addrType = 'short'
+    if 'address' in configSlotValues.keys():
+      try: 
+        addrVal = max(1,min(9999,int(configSlotValues['address'])))
+      except:
+        addrVal = 3
+    
+    if 'addressType' in configSlotValues.keys():
+      addrType = configSlotValues['addressType']
+
+    if addrType == 'short' and addrVal > 127:
+      addrType = 'long'
+
+    if addrType == 'short':
+      addrVal = 0x8000 | (0x7FFF & addrVal)
+    
+    mem[0x00] = (addrVal & 0xFF)
+    mem[0x01] = (addrVal>>8) & 0xFF
+
+    mem[0x02] = funcNameToValue(configSlotValues, 'funcHorn')
+    mem[0x03] = funcNameToValue(configSlotValues, 'funcBell')
+    mem[0x04] = funcNameToValue(configSlotValues, 'funcBrake')
+    mem[0x13] = funcNameToValue(configSlotValues, 'funcBrakeOff')
+    mem[0x05] = funcNameToValue(configSlotValues, 'funcAuxButton')
+    
+    mem[0x06] = funcNameToValue(configSlotValues, 'funcEngineOn')
+    mem[0x07] = funcNameToValue(configSlotValues, 'funcEngineOff')
+    mem[0x0A] = funcNameToValue(configSlotValues, 'funcFrontHeadlight')
+    mem[0x08] = funcNameToValue(configSlotValues, 'funcFrontDim1')
+    mem[0x09] = funcNameToValue(configSlotValues, 'funcFrontDim2')
+    
+    mem[0x0B] = funcNameToValue(configSlotValues, 'funcFrontDitchlight')
+    mem[0x0E] = funcNameToValue(configSlotValues, 'funcRearHeadlight')
+    mem[0x0C] = funcNameToValue(configSlotValues, 'funcRearDim1')
+    mem[0x0D] = funcNameToValue(configSlotValues, 'funcRearDim2')
+    mem[0x0F] = funcNameToValue(configSlotValues, 'funcRearDitchlight')
+
+
+    mem[0x10] = funcNameToValue(configSlotValues, 'funcUpButton')
+    mem[0x11] = funcNameToValue(configSlotValues, 'funcDownButton')
+    mem[0x12] = funcNameToValue(configSlotValues, 'funcThrottleUnlock')
+    mem[0x14] = funcNameToValue(configSlotValues, 'funcReverserSwap')
+    mem[0x30] = funcNameToValue(configSlotValues, 'funcCompressor')
+
+    mem[0x31] = funcNameToValue(configSlotValues, 'funcBrakeTest')
+    mem[0x32] = funcNameToValue(configSlotValues, 'funcReverserCentered')
+    mem[0x33] = funcNameToValue(configSlotValues, 'funcAlerter')
+
+    # Set notch slots to default values
+    mem[0x20] = 7;
+    mem[0x21] = 23;
+    mem[0x22] = 39;
+    mem[0x23] = 55;
+    mem[0x24] = 71;
+    mem[0x25] = 87;
+    mem[0x26] = 103;
+    mem[0x27] = 119;
+
+    for notch in range (1,9):
+      notchName = "notch%dSpeed" % notch
+      if notchName in configSlotValues.keys():
+        try:
+          mem[0x20 + notch - 1] = max(1, min(126, int(configSlotValues[notchName])))
+        except:
+          pass
+    
+    if 'brakePulseWidthMilliseconds' in configSlotValues.keys():
+      mem[0x16] = max(2, min(10, configSlotValues['brakePulseWidthMilliseconds'] / 100.0))
+
+    options = 0x01
+
+    if 'optionEStopOnBrake' in configSlotValues.keys():
+      if configSlotValues['optionEStopOnBrake'] == "On":
+        options |= 0x01
+      else:
+        options &= ~(0x01)
+
+    if 'optionReverserSwap' in configSlotValues.keys():
+      if configSlotValues['optionReverserSwap'] == "On":
+        options |= 0x02
+
+    if 'optionVariableBrake' in configSlotValues.keys():
+      if configSlotValues['optionVariableBrake'] == "On":
+        options |= 0x04
+        
+    if 'optionSteppedBrake' in configSlotValues.keys():
+      if configSlotValues['optionSteppedBrake'] == "On":
+        options |= 0x08
+        
+    mem[0x17] = options
+
+    return mem
+
+   
+  def readSlotConfig(self, mem):
+    configSlotValues = { }
+    
+    addrVal = (mem[1]<<8) + mem[0]
+    if (addrVal & 0x8000):
+      configSlotValues['addressType'] = 'short'
+      configSlotValues['address'] = addrVal & 0x7FFF
+    else:
+      configSlotValues['addressType'] = 'long'
+      configSlotValues['address'] = addrVal & 0x7FFF
+
+    configSlotValues['funcHorn'] = self.funcToName(mem[0x02])
+    configSlotValues['funcBell'] = self.funcToName(mem[0x03])
+    configSlotValues['funcBrake'] = self.funcToName(mem[0x04])
+    configSlotValues['funcBrakeOff'] = self.funcToName(mem[0x13])
+    configSlotValues['funcAuxButton'] = self.funcToName(mem[0x05])
+    configSlotValues['funcEngineOn'] = self.funcToName(mem[0x06])
+    configSlotValues['funcEngineOff'] = self.funcToName(mem[0x07])
+    configSlotValues['funcFrontHeadlight'] = self.funcToName(mem[0x0A])
+    configSlotValues['funcFrontDim1'] = self.funcToName(mem[0x08])
+    configSlotValues['funcFrontDim2'] = self.funcToName(mem[0x09])
+    configSlotValues['funcFrontDitchlight'] = self.funcToName(mem[0x0B])
+    configSlotValues['funcRearHeadlight'] = self.funcToName(mem[0x0E])
+    configSlotValues['funcRearDim1'] = self.funcToName(mem[0x0C])
+    configSlotValues['funcRearDim2'] = self.funcToName(mem[0x0D])
+    configSlotValues['funcRearDitchlight'] = self.funcToName(mem[0x0F])
+
+    configSlotValues['funcUpButton'] = self.funcToName(mem[0x10])
+    configSlotValues['funcDownButton'] = self.funcToName(mem[0x11])
+    configSlotValues['funcThrottleUnlock'] = self.funcToName(mem[0x12])
+    configSlotValues['funcReverserSwap'] = self.funcToName(mem[0x14])
+
+    configSlotValues['funcCompressor'] = self.funcToName(mem[0x30])
+    configSlotValues['funcBrakeTest'] = self.funcToName(mem[0x31])
+    configSlotValues['funcReverserCentered'] = self.funcToName(mem[0x32])
+    configSlotValues['funcAlerter'] = self.funcToName(mem[0x33])
+    
+    configSlotValues['notch1Speed'] = "%d" % mem[0x20]
+    configSlotValues['notch2Speed'] = "%d" % mem[0x21]
+    configSlotValues['notch3Speed'] = "%d" % mem[0x22]
+    configSlotValues['notch4Speed'] = "%d" % mem[0x23]
+    configSlotValues['notch5Speed'] = "%d" % mem[0x24]
+    configSlotValues['notch6Speed'] = "%d" % mem[0x25]
+    configSlotValues['notch7Speed'] = "%d" % mem[0x26]
+    configSlotValues['notch8Speed'] = "%d" % mem[0x27]
+    
+    
+    configSlotValues['brakePulseWidthMilliseconds'] = "%d" % (mem[0x16] * 100)
+    
+    if mem[0x17] & 0x01:
+      configSlotValues['optionEStopOnBrake'] = "On"
+    else:
+      configSlotValues['optionEStopOnBrake'] = "Off"
+
+    if mem[0x17] & 0x02:
+      configSlotValues['optionReverserSwap'] = "On"
+    else:
+      configSlotValues['optionReverserSwap'] = "Off"
+
+    if mem[0x17] & 0x04:
+      configSlotValues['optionVariableBrake'] = "On"
+    else:
+      configSlotValues['optionVariableBrake'] = "Off"
+
+    if mem[0x17] & 0x08:
+      configSlotValues['optionSteppedBrake'] = "On"
+    else:
+      configSlotValues['optionSteppedBrake'] = "Off"
+    
+    return configSlotValues
+
+  def help(self):
+    print("Loading interpreter for eeprom version 1.1")
---
+++ ptci.py
@@ -0,0 +1,149 @@
+# *************************************************************************
+# Title:    Protothrottle Computer Interface
+# Authors:  Nathan D. Holmes <maverick@drgw.net>
+#           Michael D. Petersen <railfan@drgw.net>
+# File:     ptci.py
+# License:  GNU General Public License v3
+#
+# LICENSE:
+#   Copyright (C) 2021 Nathan Holmes & Michael Petersen
+#    
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 3 of the License, or
+#   any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+# DESCRIPTION:
+#
+#*************************************************************************
+
+import sys
+import time
+import traceback
+import argparse
+import re
+import struct
+import mrbus
+from mrbus import packet
+import datetime
+import json
+import logging
+import sys, os, time
+
+from protothrottle import protothrottle
+
+try:
+  import serial.tools.list_ports
+except ImportError:
+  raise ImportError('serial.tools.list_ports is missing - you probably need to use pip to install serial and pySerial')
+
+def findXbeePort():
+  """This looks for the first USB serial port with an FTDI bridge chip.  In the RasPi embedded esu-bridge, this will always be the XBee."""
+  ports = list(serial.tools.list_ports.grep("ttyUSB"))
+  for p in ports:
+    if "FTDI" == p.manufacturer:
+      return p.device
+  return None
+   
+
+def main(mainParms):
+  # Unpack incoming parameters
+  # mainParms = {'startupDirectory': pwd, 'configFile': configFile, 'serialPort': args.serial, 'isDaemon':isDaemon, 'logFile':args.logfile }
+
+  throttleLetter = mainParms['throttle'][0:1]
+  if ord(throttleLetter) not in range(ord('A'), ord('Z')+1):
+    print("ERROR: Invalid throttle letter '%c'" % (throttleLetter))
+    return
+
+  mode = mainParms['mode']
+
+  throttleAddr = 0x30 + (ord(throttleLetter) - ord('A'))
+  
+  slot = int(mainParms['slot'])
+  if slot > 20 or slot < 1:
+    print("ERROR: Slot num %d out of range 1-20, aborting." % (slot))
+    return
+
+
+  serialPort = mainParms['serialPort']
+  logger = mainParms['logger']
+  mrbee = None
+
+  # Initialize MRBus / MRBee client if necessary
+  if mrbee is None:
+    try:
+      # If we didn't get the port from either of those, search for an FTDI bridge
+      #   part using findXbeePort()
+      if serialPort is None:
+        serialPort = findXbeePort()
+
+      if serialPort is None:
+        logger.warning("No XBee/MRBus interface found, waiting and retrying...")
+
+      mrbee = mrbus.mrbus(serialPort, 0xFE, logger=logger, busType='mrbee')
+      mrbee.setXbeeLED('D9', True);
+              
+    except(KeyboardInterrupt):
+      raise
+    except Exception as e:
+      if mrbee is not None:
+        mrbee.disconnect()
+        mrbee = None
+        logger.exception("Exception in starting MRBus interface")
+
+  print("MRBus Connection Established")
+
+  pt = protothrottle(mrbee)
+  pt.attach(throttleLetter)
+
+  print("PT Attached")
+  print("Global Configuration:\n----------------------------------")
+  pt.readGlobalConfig()
+  print(pt.globalConfig)
+
+
+  # Okay, what are we supposed to do here?
+  if mode == 'read':
+    slotNum = int(args.config)
+    pt.readSlot(slotNum)
+    print(pt.slots[0])
+
+
+
+  print("\nSlot 1 Configuration:\n----------------------------------")
+
+
+
+
+  
+
+
+
+
+
+
+
+if __name__ == "__main__":
+  ap = argparse.ArgumentParser()
+  ap.add_argument("-s", "--serial", help="specify serial device for XBee radio", type=str, default='/dev/ttyUSB0')
+  ap.add_argument("-t", "--throttle", help="throttle ID, A-Z", type=str, default='A')
+  ap.add_argument("-c", "--config", help="config slot number", type=int, default=1)
+  ap.add_argument("-r", "--read", help="read throttle to file", type=str, default=None)
+  ap.add_argument("-w", "--write", help="write file to throttle", type=str, default=None)
+  args = ap.parse_args()
+  logger = logging.getLogger('main')
+  # Because we might become a daemon, we need to canonicalize our path to our configuration file
+  mainParms = {'serialPort': args.serial, 'logger':logger, 'throttle':args.throttle, 'slot':args.config, 'readToFile':args.read, 'writeToThrottle':args.write, 'mode':''}
+   
+  try:
+    main(mainParms)
+  except Exception as e:
+    print(e)
+    traceback.print_exc()
+  logger.info("Terminated")
+  logging.shutdown()
